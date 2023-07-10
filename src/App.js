fefb3769c476b4e25615aaa7d12bbcbec7aa2a58
Drittu Elsa Sebastian <134041017+drittuelsa@users.noreply.github.com>
2023-07-11 02:31:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './table.css';
 import { Provider } from "react-redux";
 import { DefaultStore1 } from "./assignment/store1";
 import { ReduxTable } from "./ReduxTable";
-import { BrowserRouter, Route, Routes} from "react-router-dom";
+import { HashRouter, Route, Routes} from "react-router-dom";
 import { Update } from "./Edit";
 
 // import { Home } from "./Assignment1/Home";
@@ -20,12 +20,12 @@ function App(){
   return(
     <div>
       <Provider store ={DefaultStore1}>
-     <BrowserRouter>
+     <HashRouter>
      <Routes>
      <Route path="/" element={<ReduxTable/>}></Route>
      <Route path="/Edit/:id"  element={<Update/>}></Route>
      </Routes>
-     </BrowserRouter>
+     </HashRouter>
      {/* <Home/> */}
       </Provider>
 
